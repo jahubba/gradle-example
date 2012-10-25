109a903bc7bb6593c34775b8e4f2f611ca6cf606
jahubba <jasonahubbard@gmail.com>
2012-10-24 23:50:26 -0400

    example3

--- example3/build/gen/Hello.java
+++ example3/build/gen/Hello.java
@@ -1,35 +1,5 @@
 public class Hello {
 	public static String getMessage() {
-		return "Hello CinJug"
-	}
-}
-public class Hello {
-	public static String getMessage() {
-		return "Hello CinJug"
-	}
-}
-public class Hello {
-	public static String getMessage() {
-		return "Hello CinJug"
-	}
-}
-public class Hello {
-	public static String getMessage() {
-		return "Hello CinJug"
-	}
-}
-public class Hello {
-	public static String getMessage() {
-		return "Hello CinJug"
-	}
-}
-public class Hello {
-	public static String getMessage() {
-		return "Hello CinJug"
-	}
-}
-public class Hello {
-	public static String getMessage() {
-		return "Hello CinJug"
+		return "Hello Jason";
 	}
 }
