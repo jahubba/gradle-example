703198cc19cea1677d19210f46d51058ef975b7c
Jason <jahubba@laptop-linux.(none)>
2012-10-24 22:21:21 -0400

    initial

---
+++ example3/build/gen/Hello.java
@@ -0,0 +1,35 @@
+public class Hello {
+	public static String getMessage() {
+		return "Hello CinJug"
+	}
+}
+public class Hello {
+	public static String getMessage() {
+		return "Hello CinJug"
+	}
+}
+public class Hello {
+	public static String getMessage() {
+		return "Hello CinJug"
+	}
+}
+public class Hello {
+	public static String getMessage() {
+		return "Hello CinJug"
+	}
+}
+public class Hello {
+	public static String getMessage() {
+		return "Hello CinJug"
+	}
+}
+public class Hello {
+	public static String getMessage() {
+		return "Hello CinJug"
+	}
+}
+public class Hello {
+	public static String getMessage() {
+		return "Hello CinJug"
+	}
+}
